827eea21a7dff1568dcf596b990d1e7710566a59
Sam Hatchett <samhatchett@gmail.com>
2017-09-19 11:31:06 -0400

    tweaks lights, colors, and radius

--- training-set-generator/lights.pov
+++ training-set-generator/lights.pov
@@ -1,26 +1,26 @@
 //// Lights
 light_source {
-	<0,-101.46,-103.46>  // Latitude,Longitude,Radius: 45,0,146.315
+	<0,-201.46,-203.46>  // Latitude,Longitude,Radius: 45,0,146.315
 	color rgb <1,1,1>
 }
 light_source {
-	<109.736,-71.1575,63.3563>  // Latitude,Longitude,Radius: 30,120,146.315
+	<209.736,-171.1575,163.3563>  // Latitude,Longitude,Radius: 30,120,146.315
 	color rgb <1,1,1>
 }
 light_source {
-	<-63.3563,-124.713,36.5788>  // Latitude,Longitude,Radius: 60,-120,146.315
+	<-163.3563,-224.713,136.5788>  // Latitude,Longitude,Radius: 60,-120,146.315
 	color rgb <1,1,1>
 }
 
 light_source {
-	<0,101.46,-103.46>  
+	<0,201.46,-203.46>
 	color rgb <1,1,1>
 }
+//light_source {
+//	<209.736,171.1575,163.3563>
+//	color rgb <1,1,1>
+//}
 light_source {
-	<109.736,71.1575,63.3563>
-	color rgb <1,1,1>
-}
-light_source {
-	<-63.3563,124.713,36.5788> 
+	<-163.3563,224.713,136.5788>
 	color rgb <1,1,1>
 }
--- training-set-generator/render.py
+++ training-set-generator/render.py
@@ -3,33 +3,50 @@ import re
 import subprocess
 import numpy
 import sqlite3
+from pathlib import Path
+
+
 
 conn = sqlite3.connect('/app/inventory.db')
 c = conn.cursor()
 
+background_opt = "-b.9,.9,.9" # white background
+lights_include_opt = "-il/app/lights.pov"
+height_opt = '+H480'
+width_opt = '+W640'
 
 
 datafile_re = re.compile('\.dat', re.IGNORECASE)
 
-lights_include_opt = "-il/app/lights.pov"
+
 
 radius = -50
 lats = [-70,-50,-30,30,50,70]
 #lats = numpy.arange(-70, 70, 20)
-lons = numpy.arange(0, 350, 45)
+lons = numpy.arange(-180, 170, 45)
 
 # debug stuff
 #lats = [45]
 #lons = [90]
 
 
-# 0-15, 32-47, 256-511
-colors = numpy.append(numpy.arange(0,16,1),numpy.arange(32,48,1))
-colors = numpy.append(colors,numpy.arange(256,512,1))
+
+# get color lookups - we will use the hex values allowed by l3p
+color_lookup = {} # key: integer, value: hex color
+prefix = "0x02"
+for row in c.execute("select id, name, rgb, is_trans from colors"):
+    if row[3] is 't':
+        # transparent
+        prefix = "0x03"
+    hex_color = row[2]
+    color_id = row[0]
+    color_lookup[color_id] = {'hex': "{}{}".format(prefix, hex_color), 'name': row[1]}
+
 
 os.chdir('/data')
-subprocess.call(['rm', '-rf', '/data/*'])
-subprocess.call(['mkdir', 'img'])
+if not Path('/data','img').exists():
+    os.makedirs('/data/img')
+subprocess.call(['rm', '-rf', '/data/img/*'])
 os.chdir('/data/img')
 
 
@@ -39,29 +56,38 @@ part_rows = c.fetchall()
 for part_row in part_rows:
     os.chdir('/data/img')
     part = part_row[0]
+    # check if the part is in the ldraw library
+    part_file = Path("/opt/ldraw/parts",part).with_suffix('.dat')
+    if not part_file.is_file():
+        #something is wrong. resolve re-named parts somehow?
+        print("OOPS - part {} could not be found!".format(part))
+        continue;
+    
     subprocess.call(['mkdir', part])
     for color_row in c.execute("select distinct(color_id) from inventory_parts where part_num=:part", {"part": part}):
         color = color_row[0]
-        if color == 9999:
-            continue
+        # get hex value of color:
+        color_hex = color_lookup[color]['hex']
+        color_name = color_lookup[color]['name']
+        color_name.replace(" ", "_")
         os.chdir("/data/img/{}".format(part))
-        subprocess.call(['mkdir', "{}".format(color)])
-        os.chdir("/data/img/{}/{}".format(part, color))
-        print("rendering -- part: {} -> color: {}".format(part, color))
+        subprocess.call(['mkdir', "{}".format(color_name)])
+        os.chdir("/data/img/{}/{}".format(part, color_name))
+        print("rendering -- part: {} -> color: {}".format(part, color_name))
         fname = "{}.dat".format(part)
         for lat in lats:
             for lon in lons:
-                if lon == 180:
-                    lon = 181  # silly bug?
+                if lon == -180:
+                    lon = -179  # silly bug? sometimes camera is not pointed at part
                 pov_fname = "{}_{}_{}.pov".format(part, lat, lon)
                 out_fname = "{}_{}_{}.png".format(part, lat, lon)
                 out_fname_opt = "+O{}".format(out_fname)
-                color_opt = "-c{}".format(color)
+                color_opt = "-c{}".format(color_hex)
                 cg_opt = "-cg{},{},{}".format(lat, lon, radius)
                 subprocess.call(
-                    ['l3p', '-bWhite', '-q4', color_opt, lights_include_opt, cg_opt, '-bu', '-o', fname, pov_fname])
-                subprocess.call(['povray', '+H480', '+W640', '+A', '+Q9', '-GA', pov_fname, out_fname_opt])
-                subprocess.call(['rm', pov_fname])
+                    ['l3p', background_opt, '-q4', color_opt, lights_include_opt, cg_opt, '-bu', '-o', fname, pov_fname], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                subprocess.call(['povray', height_opt, width_opt, '+A', '+Q9', '-GA', pov_fname, out_fname_opt], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                os.remove(pov_fname)
 
 
             
@@ -75,4 +101,4 @@ for part_row in part_rows:
 
 # l3p -bWhite -q4 -c4 -cg10,45,-50 -o 27c.dat pov/ && cd pov && povray +H480 +W640 +A +Q9 *.pov
 
-        # l3p -bWhite -q4 -c4 -cg50,45,-50 -o 3755.dat && povray +H480 +W640 +A +Q9 -GA 3755.pov
+        # l3p -bWhite -q4 -c4 -cg50,45,-50 -o 11598.dat && povray +H480 +W640 +A +Q9 -GA 11598.pov
