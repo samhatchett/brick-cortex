a17c7a208de4713ea057298571519b37f1490b69
Sam Hatchett <samhatchett@gmail.com>
2017-09-21 14:25:16 -0400

    render most common first

--- training-set-generator/render.py
+++ training-set-generator/render.py
@@ -7,6 +7,7 @@ import sqlite3
 from pathlib import Path
 import shutil
 
+most_common_first = True
 
 conn = sqlite3.connect('/app/inventory.db')
 c = conn.cursor()
@@ -55,14 +56,24 @@ else:
 os.chdir('/data/img')
 
 # go through inventory and only generate images that are valid part/color combinations
-c.execute("select part_num, name from parts where part_cat_id not in (13,17,27,57)")
+#c.execute("select part_num, name from parts where part_cat_id not in (13,17,27,57)")
+#part_rows = c.fetchall()
+
+q = "select p.name, i.part_num, i.color_id, sum(i.quantity) as count from inventory_parts i join parts p USING(part_num) where p.part_cat_id not in (13,17,27,57) group by part_num, color_id order by count desc"
+# name, part color, count
+
+c.execute(q)
 part_rows = c.fetchall()
 
 # track how far we've gone
 part_num = 0
 part_count = len(part_rows)
 
-for part_row in part_rows:
+for p_row in part_rows:
+    part_name = p_row[0].encode('utf-8','ignore')
+    part = p_row[1]
+    color = p_row[2]
+
     part_num += 1
     pctDone = part_num / part_count
     print("### PART {} OF {} ( {:.3f}% complete )".format(part_num, part_count, pctDone))
@@ -83,41 +94,40 @@ for part_row in part_rows:
 
     if not Path(part).exists():
         os.mkdir(part)
-    for color_row in c.execute("select distinct(color_id) from inventory_parts where part_num=:part", {"part": part}):
-        color = color_row[0]
-        # get hex value of color:
-        color_hex = color_lookup[color]['hex']
-        color_name = color_lookup[color]['name']
-        trans = color_lookup[color]['transparent']
-        color_name.replace(" ", "_")
-        os.chdir("/data/img/{}".format(part))
-        if not Path(color_name).exists():
-            os.mkdir(color_name)
-        os.chdir("/data/img/{}/{}".format(part, color_name))
-        print("rendering -- part: {} -> color: {} ({})".format(part_name_path, color_name, "Transparent" if trans else "Opaque"))
-        sys.stdout.flush()
-        fname = "{}.dat".format(part)
-        for lat in lats:
-            for lon in lons:
-                if lon == -180:
-                    lon = -179  # silly bug? sometimes camera is not pointed at part
-                pov_fname = "{}_{}_{}.pov".format(part, lat, lon)
-                out_fname = "{}_{}_{}.png".format(part, lat, lon)
-                out_fname_opt = "+O{}".format(out_fname)
-                color_opt = "-c{}".format(color_hex)
-                cg_opt = "-cg{},{},{}".format(lat, lon, radius)
-                # does the output file already exist?
-                if Path(out_fname).exists():
-                    print("{} exists. skipping.".format(out_fname))
-                    continue
-                l3p_cmd = ['l3p', background_opt, '-q4', color_opt, lights_include_opt, cg_opt, '-bu', '-o', fname, pov_fname]
-                pov_cmd = ['povray', height_opt, width_opt, '+A', '+Q9', '-GA', pov_fname, out_fname_opt]
-                subprocess.call(l3p_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                subprocess.call(pov_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                try:
-                    os.remove(pov_fname)
-                except:
-                    print("couldn't remove {}".format(pov_fname))
+
+    # get hex value of color:
+    color_hex = color_lookup[color]['hex']
+    color_name = color_lookup[color]['name']
+    trans = color_lookup[color]['transparent']
+    color_name.replace(" ", "_")
+    os.chdir("/data/img/{}".format(part))
+    if not Path(color_name).exists():
+        os.mkdir(color_name)
+    os.chdir("/data/img/{}/{}".format(part, color_name))
+    print("rendering -- part: {} -> color: {} ({})".format(part_name_path, color_name, "Transparent" if trans else "Opaque"))
+    sys.stdout.flush()
+    fname = "{}.dat".format(part)
+    for lat in lats:
+        for lon in lons:
+            if lon == -180:
+                lon = -179  # silly bug? sometimes camera is not pointed at part
+            pov_fname = "{}_{}_{}.pov".format(part, lat, lon)
+            out_fname = "{}_{}_{}.png".format(part, lat, lon)
+            out_fname_opt = "+O{}".format(out_fname)
+            color_opt = "-c{}".format(color_hex)
+            cg_opt = "-cg{},{},{}".format(lat, lon, radius)
+            # does the output file already exist?
+            if Path(out_fname).exists():
+                print("{} exists. skipping.".format(out_fname))
+                continue
+            l3p_cmd = ['l3p', background_opt, '-q4', color_opt, lights_include_opt, cg_opt, '-bu', '-o', fname, pov_fname]
+            pov_cmd = ['povray', height_opt, width_opt, '+A', '+Q9', '-GA', pov_fname, out_fname_opt]
+            subprocess.call(l3p_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.call(pov_cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            try:
+                os.remove(pov_fname)
+            except:
+                print("couldn't remove {}".format(pov_fname))
 
 
             
